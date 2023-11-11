c97437db97fb6b6d58adf8ce981017ada3aad4fa
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2023-11-11 16:57:05 +0530

    Time: 42 ms (19.55%), Space: 10.6 MB (38.75%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             int count=0;
             for(int j=0;j<nums.size();j++){
-                if(i!=j && nums[i]>nums[j])count++;
+                if(nums[i]>nums[j])count++;
             }
             result.push_back(count);
         }
