fd860897843e3e8ab6f38bade408ad46f003910d
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2023-11-12 12:46:28 +0530

    Time: 7 ms (86.45%), Space: 10.7 MB (18.92%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int mostWordsFound(std::vector<std::string>& s) {
+        std::vector<int> lengths;
+        for (int i = 0; i < s.size(); i++) {
+            std::string str = s.at(i);
+            int count = 0;
+            for (int j = 0; j < str.size(); j++) {
+                if (str.at(j) == ' ') {
+                    count++;
+                }
+            }
+            lengths.push_back(count + 1);
+        }
+
+        int max_length = lengths[0];
+        for (int i = 0; i < lengths.size(); i++) {
+            if (lengths[i] > max_length) {
+                max_length = lengths[i];
+            }
+        }
+        return max_length;
+    }
+};
