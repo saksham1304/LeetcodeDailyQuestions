628d216e79c923a28d64a274c3fb243041f59e8e
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2023-12-01 18:23:39 +0530

    Time: 8 ms (98.51%), Space: 24.3 MB (72.00%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,33 +2,22 @@
 
 class Solution {
 public:
-    std::vector<int> productExceptSelf(std::vector<int>& a) {
-        std::vector<int> result;
-        int n = a.size();
-
-        // Initialize an array to store the product of all elements to the left of each index
-        std::vector<int> leftProducts(n, 1);
-
-        // Initialize an array to store the product of all elements to the right of each index
-        std::vector<int> rightProducts(n, 1);
+    std::vector<int> productExceptSelf(std::vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> result(n, 1);
 
         // Compute the product of all elements to the left of each index
         int leftProduct = 1;
-        for (int i = 1; i < n; i++) {
-            leftProduct *= a[i - 1];
-            leftProducts[i] = leftProduct;
-        }
-
-        // Compute the product of all elements to the right of each index
-        int rightProduct = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            rightProduct *= a[i + 1];
-            rightProducts[i] = rightProduct;
-        }
-
-        // Multiply the corresponding left and right products to get the final result
         for (int i = 0; i < n; i++) {
-            result.push_back(leftProducts[i] * rightProducts[i]);
+            result[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // Compute the product of all elements to the right of each index and multiply with the left product
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
 
         return result;
