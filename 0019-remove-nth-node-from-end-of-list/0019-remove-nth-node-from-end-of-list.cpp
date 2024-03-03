8f14b944b01c7ffd8ea04a2b894e868be1d491d7
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-03-03 12:08:48 +0530

    Time: 0 ms (100.00%), Space: 13.2 MB (45.90%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,34 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (head == nullptr) return nullptr;
-        
-        // Create a dummy node to handle edge cases when removing the head
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        
-        ListNode* fast = dummy;
-        ListNode* slow = dummy;
-        
-        // Move the fast pointer n steps ahead
-        for (int i = 0; i < n; ++i) {
-            fast = fast->next;
+         ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast=head;
+         ListNode* slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
-        
-        // Move both pointers until the fast pointer reaches the end
-        while (fast->next != nullptr) {
-            fast = fast->next;
-            slow = slow->next;
+               if(fast==nullptr)return head->next;
+        while(fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next;
         }
-        
-        // Remove the nth node from the end
-        ListNode* toDelete = slow->next;
-        slow->next = slow->next->next;
-        delete toDelete;
-        
-        // Return the updated head
-        ListNode* result = dummy->next;
-        delete dummy;
-        return result;
+        ListNode* delnode=slow->next;
+        slow->next=slow->next->next;
+        delete(delnode);
+        return head;
     }
 };
