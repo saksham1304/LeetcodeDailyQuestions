2c64d7d654f4e8988a488ba85b2563007589e543
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-07-03 17:27:48 +0530

    Time: 5 ms (50.00%), Space: 9 MB (80.37%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) { 
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+        return dp;
+    }
+};
