31bb3954f7b5820c77ff8099d7a2f5bb3e1d8c8f
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-07-11 22:37:10 +0530

    Time: 4 ms (32.53%), Space: 9.6 MB (11.85%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int maxnonadj(vector<int>&n){
+        if (n.empty()) return 0;  
+        if (n.size() == 1) return n[0]; 
+        int prev=n[0];
+        int prev2=0;
+        for(int i=1;i<n.size();i++){
+            int take=n[i];
+            if(i>1)take+=prev2;
+            int nontake=0+prev;
+            int curri=max(take,nontake);
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if (n == 1) return nums[0];
+        vector<int>temp1,temp2;
+        for(int i=0;i<n;i++){
+            if(i!=0)temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+        }
+        return max(maxnonadj(temp1),maxnonadj(temp2));
+    }
+};
