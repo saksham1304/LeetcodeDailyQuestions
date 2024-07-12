08aec8668aeb0f5f712e43fc5555ac59281565f9
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-07-13 00:00:25 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (17.58%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int func(int m , int n, vector<vector<int>>&dp){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 &&j==0){
+                    dp[i][j]=1;
+                    continue;}
+                    int up=0,left=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)left=dp[i][j-1];
+                    dp[i][j]=left+up;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return func(m,n,dp);
+    }
+};
