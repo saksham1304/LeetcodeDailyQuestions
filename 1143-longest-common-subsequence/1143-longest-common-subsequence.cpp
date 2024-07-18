044656a1062ddd73fc230dd26a4bd7d2fc844982
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-07-18 12:16:25 +0530

    Time: 20 ms (74.83%), Space: 26 MB (14.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,19 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.length();
-        int n = text2.length();
-
-        // Create a 2D vector to store the length of LCS for each pair of substrings
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        // Fill the dp table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int j=0;j<=m;j++)dp[0][j]=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]= 0+max(dp[i-1][j],dp[i][j-1]);
             }
         }
-
-        // The bottom-right cell of the dp table contains the length of LCS
-        return dp[m][n];
+        return dp[n][m];
     }
 };
