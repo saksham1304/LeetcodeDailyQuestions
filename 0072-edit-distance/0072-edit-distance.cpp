ff65aec17c3a4e26e17580be47d14bb591d78f3d
Saksham Gupta <97667982+saksham1304@users.noreply.github.com>
2024-07-19 12:04:55 +0530

    Time: 9 ms (48.78%), Space: 13 MB (21.55%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDistance(string w1, string w2) {
+        int n=w1.size();
+        int m=w2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(w1[i-1]==w2[j-1])dp[i][j]=0+dp[i-1][j-1];
+                else 
+                    dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
+    }
+};
